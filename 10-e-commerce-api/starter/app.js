62642229aaef150a3b8262aeafa4e09c79c40f68
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-17 06:50:14 +0100

    setting up the middlewares

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,42 @@
-console.log('E-Commerce API');
+require ('dotenv').config();
+require('express-async-errors');
+const express =  require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+// DATABASE
+const connectDB = require('./db/connect');
+
+// MIDDLEWARE
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//rest of the package
+app.use(morgan('tiny'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('e-commerce')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+const start = async () => {
+    try {
+
+        await connectDB(process.env.MONGODB_URI);
+        app.listen(port, () => { 
+            console.log(`App is running on port ${port}...`);
+            
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start();
--- 10-e-commerce-api/starter/db/connect.js
+++ 10-e-commerce-api/starter/db/connect.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
+  return mongoose.connect(url)
+    .then(() => console.log('Database connected successfully'))
+    .catch((err) => console.error('Database connection error:', err));
 };
 
 module.exports = connectDB;
