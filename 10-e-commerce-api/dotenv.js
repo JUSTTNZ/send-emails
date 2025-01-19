74d0ff3bd93a8537f108a94e3b8afcbd08cac978
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-19 22:31:44 +0100

    .

--- 10-e-commerce-api/dotenv.js
+++
@@ -1,4 +0,0 @@
-MONGODB_URI = mongodb+srv://NZDdev:Belligerency01@cluster0.prny1.mongodb.net/eCommerceDB2
-JWT_SECRET = KiLkY/dU8Cxvn30C2wjEW2E7xKSCXZqfQ20Daqi/LizN4JPVvnzJgPpQq4fD30vD
-uqSrNj+DscW5tYKwDmGpTg
-JWT_LIFETIME = 1d
