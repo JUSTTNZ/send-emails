acbf0341b631d57f222e47acb836c153badf4975
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-19 07:04:06 +0100

    fix: update cookie parser to use JWT secret and enhance login error handling

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -21,11 +21,33 @@ const register = async(req, res) => {
 };
 
 const login = async(req, res) => {
-    res.send('login')
-};
+    const {email, password} = req.body;
+    if(!email || !password) {
+        throw new CustomError.BadRequestError('Please provide email and password')
+    }
+
+    const user =  await User.findOne({ email });
+
+    if(!user) {
+        throw new CustomError.UnauthenticatedError('Invalid credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError('Invalid credentials')
+    }
+
+    const tokenUser = {name:user.name, userId: user._id, role: user.role}
+    attachCookiesToResponse({res, user: tokenUser})
+    res.status(StatusCodes.CREATED).json({ user: tokenUser})
+}
 
 const logout = async(req, res) => {
-    res.send('logout')
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expiresIn: new Date(Date.now() + 5 * 1000),
+    })
+    res.status(StatusCodes.OK).json({message: 'logged out'})
 };
 
 module.exports = {
