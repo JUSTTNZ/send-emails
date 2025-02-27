135c24edb2f780cb0e8b70948e1ae14b9b234652
CHUKWUDI CONFIDENCE OSINACHI <chukwudiconfidenceosinachi@gmail.com>
2025-02-27 22:22:54 +0100

    Update stripeController.js

--- 09-stripe-payment/starter/controllers/stripeController.js
+++ 09-stripe-payment/starter/controllers/stripeController.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const stripeController = async(req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     
     const { purchase, total_amount, shipping_fee } = req.body;
 
